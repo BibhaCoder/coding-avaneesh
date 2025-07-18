66beaedde54efd6c093077234f218a223507527c
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2025-07-18 12:33:46 -0700

    Update bitwise-right-shift.py

--- Chapter 2/bitwise-right-shift.py
+++ Chapter 2/bitwise-right-shift.py
@@ -1,7 +1,7 @@
 user_input = int(input("Enter a number"))
-left_shift = int(input("Enter shift"))
-#shifts bits to the right by left_shift, each shift divides number by 2 and makes numbers to the left of shift 0
-answer = user_input >> left_shift
+right_shift = int(input("Enter shift"))
+#shifts bits to the right by right_shift, each shift divides number by 2 and makes numbers to the left of shift 
+answer = user_input >> right_shift
 print answer
   
 
