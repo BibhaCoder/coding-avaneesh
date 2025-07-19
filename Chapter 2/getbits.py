26cd299964a91ad4260af17fcdacff853d3c2971
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2025-07-19 15:15:38 -0700

    Update getbits.py

--- Chapter 2/getbits.py
+++ Chapter 2/getbits.py
@@ -3,7 +3,9 @@ def getbits():
     p = int(input())
     n = int(input())
     print(bin(x))
-    
+    if n > p + 1:
+      print("Invalid n: " + str(n))
+      return
     mask = (~(~0 << n))
     y = (x >> (p + 1 - n) & mask)
     print (bin(y))
