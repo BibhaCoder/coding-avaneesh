c847f6fbb937ee4f57b08a0f979222151d72339b
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2025-07-23 11:58:41 -0700

    Update setbits.py

--- Chapter 2/setbits.py
+++ Chapter 2/setbits.py
@@ -5,9 +5,12 @@ def setbits():
   y = int(input())
   y_mask = (~(~0 << n))
   print bin(y)
-  print bin(y_mask)
   y &= y_mask
-  print bin(y)
-  x_mask = ~(~0 << n) << (p + 1 - n)
+  x_mask = (~(~0 << n) << (p + 1 - n))
+  print bin(x_mask)
+  x = x & (~x_mask)
+  x = x | y << (p + 1 - n)
+  print bin(x)
 
 setbits()
+
