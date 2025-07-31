f3410386ebf28a85e691d64c8580cafef708c257
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2025-07-30 21:58:46 -0700

    Create count_char_digit_and_space.py

---
+++ Chapter 3/count_char_digit_and_space.py
@@ -0,0 +1,17 @@
+user_input = input()
+
+
+digit_count = [char for char in user_input if char.isdigit()]
+letter_count = [char for char in user_input if char.isalpha()]
+digits_array = [0] * 10
+
+
+digit_count = len(digit_count)
+letter_count = len(letter_count)
+
+print("Amount of Digits:", digit_count)
+print("Amount of Letters:", letter_count)
+
+white_space_count = len(user_input) - digit_count - letter_count
+
+print("Amount of white spaces: ", white_space_count)
