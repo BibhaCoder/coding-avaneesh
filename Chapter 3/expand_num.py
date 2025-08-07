fe5bc6821c1cf766ed4e04ca8051917bda76c555
BibhaCoder <47375509+BibhaCoder@users.noreply.github.com>
2025-08-06 21:33:26 -0700

    Create expand_num.py

---
+++ Chapter 3/expand_num.py
@@ -0,0 +1,19 @@
+def expand():
+  
+    s1 = int(input("Enter start number: "))
+    s2 = int(input("Enter end number: "))
+  
+    for i in range(s1, s2 + 1):
+      print(i)
+      
+    
+      
+      
+    
+    
+    
+
+
+expand()
+
+
